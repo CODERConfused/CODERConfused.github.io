3dd99fd3b05a5064e301e93f2dfb7379fafa112a
CODERConfused <155583957+CODERConfused@users.noreply.github.com>
2025-04-06 11:28:10 -0400

    fixed rain bug

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const createRain = () => {
         drop.style.left = Math.random() * 100 + 'vw';
         drop.style.animationDuration = Math.random() * 2 + 2 + 's';
         drop.style.animationDelay = Math.random() * 2 + 's';
+        drop.style.transform = 'translateY(-100vh)'; // Start off-screen
         body.appendChild(drop);
     }
 };
